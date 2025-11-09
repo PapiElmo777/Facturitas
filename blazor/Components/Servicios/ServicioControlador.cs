79a945fc91df719dcc3065589b41cd0d893125f2
Alfredo <ajc7873@gmail.com>
2025-11-09 14:53:31 -0700

    Agregue la lista donde van a estar las facturas, solo me falta agregar el metodo de cargar factura

---
+++ blazor/Components/Servicios/ServicioControlador.cs
@@ -0,0 +1,17 @@
+using blazor.Components.Data;
+using System.Collections.Generic;
+
+namespace blazor.Components.Servicios
+{
+    public class ServicioControlador
+    {
+        private readonly ServicioFacturas _servicioFacturas;
+        public List<Factura> facturas { get; private set; }
+        public ServicioControlador(ServicioFacturas servicioFacturas)
+        {
+            _servicioFacturas = servicioFacturas;
+            CargarFacturas();
+        }
+    }
+    
+}
