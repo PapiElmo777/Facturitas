55c8db7f9987a9c6bc24acbed8b0403a32acc49a
Alfredo <ajc7873@gmail.com>
2025-11-10 05:46:09 -0700

    Funciona la paginita

--- blazor/obj/Debug/net9.0/blazor.AssemblyInfo.cs
+++ blazor/obj/Debug/net9.0/blazor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("blazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b62a1ea6a438f9b7d2aae9a52c26b81fa2b0b5c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b32441fca756858721acb28dfe9207ec55e8ecc2")]
 [assembly: System.Reflection.AssemblyProductAttribute("blazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("blazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
