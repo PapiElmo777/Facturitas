0404bcda39dd06c49b7b98c56a864def12d8f9be
Alfredo <ajc7873@gmail.com>
2025-11-10 14:08:57 -0700

    modifique el metodo de Actualizar o agregar, por error al agregar articulos

--- blazor/obj/Debug/net9.0/blazor.AssemblyInfo.cs
+++ blazor/obj/Debug/net9.0/blazor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("blazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2643f6195fc24b6418bbe0e5d516f897554f1cec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41b5822a3ef59660b7fdfae95612897e9e9184f0")]
 [assembly: System.Reflection.AssemblyProductAttribute("blazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("blazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
