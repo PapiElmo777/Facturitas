9de7e2fa3379230b097b75ebdcecaa956f666813
Alfredo <ajc7873@gmail.com>
2025-11-12 13:34:12 -0700

    Agregue los metodos de actualizar y de eliminar facturas al SC

--- blazor/Components/Servicios/ServicioControlador.cs
+++ blazor/Components/Servicios/ServicioControlador.cs
@@ -27,6 +27,21 @@ namespace blazor.Components.Servicios
             _servicioFacturas.AddFactura(factura);
             CargarFacturas();
         }
+        public void ActualizarFactura(Factura factura)
+        {
+            if (factura.Items == null || factura.Items.Count == 0)
+            {
+                Console.WriteLine("La factura debe tener al menos un artículo.");
+                return;
+            }
+            _servicioFacturas.UpdateFactura(factura);
+            CargarFacturas();
+        }                
+        public void EliminarFactura(int id)
+        {
+            _servicioFacturas.DeleteFactura(id);
+            CargarFacturas();
+        }
     }
     
 }
